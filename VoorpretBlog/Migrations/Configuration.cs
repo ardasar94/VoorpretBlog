4f1c3c3a9f13d719db0169b908ab841a6a48380e
ardasar94 <sarihanarda@gmail.com>
2021-04-05 16:25:42 +0300

    Models creation

---
+++ VoorpretBlog/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VoorpretBlog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VoorpretBlog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VoorpretBlog.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ VoorpretBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VoorpretBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public List<string> Tags { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+        public virtual ICollection<Like> Likes { get; set; }
+
+    }
+}
