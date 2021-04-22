01b29506e1b8c31ed0d3f423bbd66d58cadf1e17
ardasar94 <sarihanarda@gmail.com>
2021-04-22 16:24:38 +0300

    Home Page and Pagination

---
+++ VoorpretBlog/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoorpretBlog.Models
+{
+    public class HomeViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public List<Post> FullPosts { get; set; }
+
+        public PaginationInfoViewModel PaginationInfo { get; set; }
+    }
+}
